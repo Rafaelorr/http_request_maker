425a92249a34898a46b1537208faca43a83bd97e
Rafaelorr <rafael.onkelinx@muscardinus.be>
2025-12-29 15:07:25 +0100

    hernoem main.py naar http_get_tool.py

--- http_get_tool.py
+++ http_get_tool.py
@@ -2,20 +2,29 @@ import requests
 import json
 import re
 
-# api-endpoint
-url = input("Typ het API endpoint URL: ")
+def is_valid(json_string):
+    print("JSON String:", json_string)
+    try:
+        json.loads(json_string)
+        print("  Is valid?: True")
+    except ValueError:
+        print("  Is valid?: False")
+        return None
 
-pattern = r'https?://\S+|www\.\S+'
-if not re.findall(pattern,url):
+# api-endpoint
+while True:
+    url = input("Typ het API endpoint URL: ")
+    pattern = r'https?://\S+|www\.\S+'
+    if re.findall(pattern,url):
+        break
     print("Ongeldige URL. Zorg ervoor dat de URL begint met http:// of https://")
     url = input("Typ het API endpoint URL: ")
 
 while True:
     param_file = input("Typ de bestandsnaam van de parameters (bijv. params.json): ")
-    if param_file.strip():
+    if param_file.strip() and is_valid(param_file):
         break
-    else:
-        print("Type een geldige bestandsnaam.")
+    print("Type een geldig json bestandsnaam.")
 
 with open(param_file, "r") as f:
     params = json.load(f)
