044ddbf61afdc296866651cd53426d2c517249ed
Rafaelorr <rafael.onkelinx@muscardinus.be>
2026-01-17 11:43:28 +0100

    voeg de test server json data toe aan een config

--- test_server.py
+++ test_server.py
@@ -1,17 +1,20 @@
 from flask import Flask, jsonify
+import json
+
+try:
+    with open('get_server_config.json', 'r') as file:
+        data = json.load(file)
+    print("File data =", data)
+    
+except FileNotFoundError:
+    print("Error: Het bestand 'get_server_config.json' bestaat niet.")
 
 app = Flask(__name__)
 
 @app.route('/api/get', methods=['GET'])
 def main_get():
-    data = {
-        "status": "success",
-        "message": "Dit is een test API endpoint",
-        "data": {
-            "id": 1,
-            "naam": "Test data"
-        }
-    }
+    if not data:
+        return 404
     return jsonify(data)
 
 if __name__ == '__main__':
